563a321be36e07d5e58943f3010252cd7fd99bdc
Dev-Kings <kingasiadavid41@gmail.com>
2022-12-16 09:05:39 +0300

    created categories, makers, products api

---
+++ products/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import include, path
+
+from .views import MakerListView, ProductCategoryListView, ProductsListView
+
+app_name = "products"
+
+urlpatterns = [
+    path("", ProductsListView.as_view(), name="products-list"),
+    path("categories", ProductCategoryListView.as_view(), name="categories-list"),
+    path("makers", MakerListView.as_view(), name="makers-list"),
+]
